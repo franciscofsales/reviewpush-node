4a741a9557db2dba4ddc938aa6ee78c4bd140c1e
Francisco Sales <franciscofsales1@gmail.com>
2018-11-20 19:28:18 +0000

    chore: change name of methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import Auth from './auth';
-import Location from './location';
+import Locations from './location';
 import Reviews from './review';
 
 export default class ReviewPush {
   public Reviews: typeof Reviews;
-  public Location: typeof Location;
+  public Locations: typeof Locations;
   constructor(props: { email: string; password: string }) {
     if (!props || !props.email || !props.password) {
       throw new Error('Invalid or missing credentials');
     }
     Auth.setCredentials(props.email, props.password);
     this.Reviews = Reviews;
-    this.Location = Location;
+    this.Locations = Locations;
   }
 }
